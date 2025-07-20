6d3edb0eee5ae29b2625ee2125ba0ee394614ade
Giorgio <djorkam@gmail.com>
2025-07-21 01:28:13 +0200

    Fix CORNR App Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Header } from './components/Header';
 import { Navigation } from './components/Navigation';
 import { HomePage } from './components/HomePage';
